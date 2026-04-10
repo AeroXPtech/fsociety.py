ee32b9c56f43a483acd8c6deb200a8875fd200cd
user <aryanzareai31@gmail.com>
2026-04-10 21:18:59 +0200

    WORKS NOW
    finally

--- fsociety.py
+++ fsociety.py
@@ -1,20 +1,20 @@
+import time
 print("----------------------")
 print("        Index.py      ")
 print("      by techyx32     ")
 print("  and fsociety_linux  ")
 print("       0n T1kT0k      ")
 print("----------------------")
-
 while True:
     print("\n--------------------")
     print("help - show commands")
     print("--------------------")
 
-    choice = input("┌──(fsociety㉿kali)-[~]
-└─$  ").lower
+    choice = input("""┌──(fsociety㉿kali)
+└─$ """).lower()
 
     if choice == "help":
-        print("------------------------")
+        print("\n------------------------")
         print(" fastfetch - show specs ")
         print("  github - show source  ")
         print("  infos - show updates  ")
@@ -24,32 +24,32 @@ while True:
 
     elif choice == "fastfetch":
         print("""
-       ..............                                      fsociety@kali
-            ..,;:ccc,.                              -----------------
-          ......''';lxO.                            OS: Kali GNU/Linux Rolling x86_64
-.....''''..........,:ld;                            Host: 20W1S7PR18 (ThinkPad T14 Gen 2i)
-           .';;;:::;,,.x,                           Kernel: Linux 6.18.12+kali-amd64
-      ..'''.            0Xxoc:,.  ...               Uptime: 2 hours, 11 mins
-  ....                ,ONkc;,;cokOdc',.             Packages: 3568 (dpkg)
- .                   OMo           ':ddo.           Shell: zsh 5.9
-                    dMc               :OO;          Display (AUO323D): 1920x1080 in 14", 60 Hz [Built-in]
-                    0M.                 .:o.        DE: Xfce4 4.20
-                    ;Wd                             WM: Xfwm4 (X11)
-                     ;XO,                           WM Theme: Kali-Dark
-                       ,d0Odlc;,..                  Theme: Fusion [Qt], Kali-Dark [GTK2/3/4]
-                           ..',;:cdOOd::,.          Icons: Flat-Remix-Blue-Dark [Qt], Flat-Remix-Blue-Dark [GTK2/3/4]
-                                    .:d;.':;.       Font: Cantarell (11pt) [GTK2/3/4]
-                                       'd,  .'      Cursor: Adwaita (24px)
-                                         ;l   ..    Terminal: qterminal 2.3.0
-                                          .o        Terminal Font: FiraCode (10pt)
-                                            c       CPU: 11th Gen Intel(R) Core(TM) i5-1145G7 (8) @ 4.40 GHz
-                                            .'      GPU: Intel Iris Xe Graphics @ 1.30 GHz [Integrated]
-                                             .      Memory: 2.06 GiB / 15.33 GiB (13%)
-                                                    Swap: 0 B / 12.30 GiB (0%)
-                                                    Disk (/): 93.97 GiB / 220.63 GiB (43%) - ext4
-                                                    Local IP (wlan0): 192.168.178.35/24
-                                                    Battery (5B10W51826): 100% [Discharging]
-                                                    Locale: nl_NL.UTF-8
+        ..............                                      fsociety@kali
+             ..,;:ccc,.                              -----------------
+           ......''';lxO.                            OS: Kali GNU/Linux Rolling x86_64
+ .....''''..........,:ld;                            Host: 20W1S7PR18 (ThinkPad T14 Gen 2i)
+            .';;;:::;,,.x,                           Kernel: Linux 6.18.12+kali-amd64
+       ..'''.            0Xxoc:,.  ...               Uptime: 2 hours, 11 mins
+   ....                ,ONkc;,;cokOdc',.             Packages: 3568 (dpkg)
+  .                   OMo           ':ddo.           Shell: zsh 5.9
+                     dMc               :OO;          Display (AUO323D): 1920x1080 in 14", 60 Hz [Built-in]
+                     0M.                 .:o.        DE: Xfce4 4.20
+                     ;Wd                             WM: Xfwm4 (X11)
+                      ;XO,                           WM Theme: Kali-Dark
+                        ,d0Odlc;,..                  Theme: Fusion [Qt], Kali-Dark [GTK2/3/4]
+                            ..',;:cdOOd::,.          Icons: Flat-Remix-Blue-Dark [Qt], Flat-Remix-Blue-Dark [GTK2/3/4]
+                                     .:d;.':;.       Font: Cantarell (11pt) [GTK2/3/4]
+                                        'd,  .'      Cursor: Adwaita (24px)
+                                          ;l   ..    Terminal: qterminal 2.3.0
+                                           .o        Terminal Font: FiraCode (10pt)
+                                             c       CPU: 11th Gen Intel(R) Core(TM) i5-1145G7 (8) @ 4.40 GHz
+                                             .'      GPU: Intel Iris Xe Graphics @ 1.30 GHz [Integrated]
+                                              .      Memory: 2.06 GiB / 15.33 GiB (13%)
+                                                     Swap: 0 B / 12.30 GiB (0%)
+                                                     Disk (/): 93.97 GiB / 220.63 GiB (43%) - ext4
+                                                     Local IP (wlan0): 192.168.178.35/24
+                                                     Battery (5B10W51826): 100% [Discharging]
+                                                     Locale: nl_NL.UTF-8
 
                                                                             
                                                                             """)
@@ -62,7 +62,7 @@ while True:
 
     elif choice == "exit":
         print("\nGood Bye!")
-        break
+        exit()
 
     elif choice == "ddos":
        print("choose Website (ex. usa.gov)")
@@ -126,6 +126,7 @@ while True:
        print("DDoSing " + site + ". 200 OK")
        time.sleep(5)
        print("DDoS finished! 503 Service Unavailable")
+       time.sleep(2)
        
     else:
         print("\nIllegal Command")
